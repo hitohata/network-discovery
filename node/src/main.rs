b7917bf73cdb2f9f8f4d72e089dded03e60158af
hitohata <hirohatatro@gmail.com>
2025-08-02 12:00:11 -0700

    feat: updated the structure

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,7 +1,4 @@
-use shared::scan::usage;
-use shared::schemas;
-use std::net::UdpSocket;
-use tracing::{error, info, Level};
+use tracing::Level;
 
 fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt()
@@ -10,54 +7,10 @@ fn main() -> std::io::Result<()> {
         .with_thread_names(true)
         .init();
 
-    let socket = UdpSocket::bind(format!("0.0.0.0:{}", shared::utils::constants::TARGET_PORT))?;
-    socket.set_broadcast(true)?;
-    info!("Starting HTTP server on {}", socket.local_addr()?);
+    let target_server = shared::server::target_server::TargetServer::new();
 
-    let system = usage::SystemInfo::default();
-
-    let mut buf = [0; 1024];
-
-    loop {
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        let received_data = &buf[..amt];
-
-        let Ok(request) = serde_json::from_slice::<schemas::manager_messages::ManagerRequestSchema>(
-            received_data,
-        ) else {
-            error!(
-                "Failed to parse received data from {}: {:?}",
-                src,
-                String::from_utf8_lossy(received_data)
-            );
-            continue;
-        };
-
-        match request {
-            schemas::manager_messages::ManagerRequestSchema::Spec(req) => {
-                info!("Received Spec request from {}: {:?}", src, req);
-                let response = schemas::target_messages::SpecResponse::new(
-                    system.get_machine_info().to_owned(),
-                );
-                let Ok(response_data) = serde_json::to_vec(&response) else {
-                    error!("Failed to serialize response for Spec request from {}", src);
-                    continue;
-                };
-                socket.send_to(&response_data, src)?;
-            }
-            schemas::manager_messages::ManagerRequestSchema::UsageOverview(req) => {
-                info!("Received Usage Overview request from {}: {:?}", src, req);
-                let response =
-                    schemas::target_messages::UsageOverviewResponse::new(system.get_usage());
-                let Ok(response_data) = serde_json::to_vec(&response) else {
-                    error!(
-                        "Failed to serialize response for Usage Overview request from {}",
-                        src
-                    );
-                    continue;
-                };
-                socket.send_to(&response_data, src)?;
-            }
-        }
-    }
+    target_server.run().map_err(|e| {
+        tracing::error!("Failed to run TargetServer: {}", e);
+        e
+    })
 }
---
+++ shared/src/server/target_server.rs
@@ -0,0 +1,76 @@
+//! this is server side code for the target
+//! this function
+
+use crate::scan::usage;
+use crate::schemas;
+use std::net::UdpSocket;
+use tracing::{error, info};
+
+pub struct TargetServer {
+    system_info: usage::SystemInfo,
+}
+
+impl TargetServer {
+    pub fn new() -> Self {
+        let system_info = usage::SystemInfo::default();
+        Self { system_info }
+    }
+
+    pub fn run(&self) -> std::io::Result<()> {
+        let socket = UdpSocket::bind(format!("0.0.0.0:{}", crate::utils::constants::TARGET_PORT))?;
+        socket.set_broadcast(true)?;
+        info!("Starting HTTP server on {}", socket.local_addr()?);
+
+        let mut buf = [0; 1024];
+
+        loop {
+            let (amt, src) = socket.recv_from(&mut buf)?;
+            let received_data = &buf[..amt];
+
+            let Ok(request) = serde_json::from_slice::<
+                schemas::manager_messages::ManagerRequestSchema,
+            >(received_data) else {
+                error!(
+                    "Failed to parse received data from {}: {:?}",
+                    src,
+                    String::from_utf8_lossy(received_data)
+                );
+                continue;
+            };
+
+            match request {
+                schemas::manager_messages::ManagerRequestSchema::Spec(req) => {
+                    info!("Received Spec request from {}: {:?}", src, req);
+                    let response = schemas::target_messages::SpecResponse::new(
+                        self.system_info.get_machine_info().to_owned(),
+                    );
+                    let Ok(response_data) = serde_json::to_vec(&response) else {
+                        error!("Failed to serialize response for Spec request from {}", src);
+                        continue;
+                    };
+                    socket.send_to(&response_data, src)?;
+                }
+                schemas::manager_messages::ManagerRequestSchema::UsageOverview(req) => {
+                    info!("Received Usage Overview request from {}: {:?}", src, req);
+                    let response = schemas::target_messages::UsageOverviewResponse::new(
+                        self.system_info.get_usage(),
+                    );
+                    let Ok(response_data) = serde_json::to_vec(&response) else {
+                        error!(
+                            "Failed to serialize response for Usage Overview request from {}",
+                            src
+                        );
+                        continue;
+                    };
+                    socket.send_to(&response_data, src)?;
+                }
+            }
+        }
+    }
+}
+
+impl Default for TargetServer {
+    fn default() -> Self {
+        Self::new()
+    }
+}
